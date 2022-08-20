c70724e7f631b450fa44958e5b81cc34e79d660d
Dimaregaisia1749 <dmeln1749@gmail.com>
2022-08-20 09:46:40 +0300

    add loading save

---
+++ interface.py
@@ -0,0 +1,84 @@
+import pygame
+
+
+FONT = "fonts/New Zelek.ttf"
+MAIN_FONT_SIZE = 80
+SLIDER_FONT_SIZE = 25
+RADIO_BUTTON_FONT_SIZE = 30
+
+
+class UserInterface:
+    def __init__(self, path):
+        self.image = pygame.image.load(path)
+
+
+class Background(UserInterface):
+    def render(self, game_manager):
+        game_manager.display.blit(self.image, self.image.get_rect())
+
+
+class Button(UserInterface):
+    def __init__(self, x=0, y=0, weight=800, height=150, text='sample', color=(0, 0, 0), path='images/menu/button.png'):
+        super().__init__(path)
+        self.weight = weight
+        self.height = height
+        self.font = pygame.font.Font(FONT, MAIN_FONT_SIZE)
+        self.text_box = self.font.render(text, True, color)
+        self.x, self.y = x, y
+
+    def render(self, game_manager):
+        image_rect = self.image.get_rect()
+        text_box_rect = self.text_box.get_rect()
+        game_manager.display.blit(self.image, (self.x, self.y))
+        game_manager.display.blit(self.text_box, (self.x + (image_rect.width - text_box_rect.width) // 2,
+                                                  (self.y + (image_rect.height - text_box_rect.height) // 2)))
+
+    def check_click(self, event):
+        if event.button == 1:
+            if self.x <= pygame.mouse.get_pos()[0] <= self.x + self.weight:
+                if self.y <= pygame.mouse.get_pos()[1] <= self.y + self.height:
+                    return True
+        return False
+
+
+class Slider(UserInterface):
+    def __init__(self, x=0, y=0, weight=800, height=20, text='sample', color=(0, 0, 0), value=100, max_value=100,
+                 path_base='images/menu/slider base.png', path_circle='images/menu/slider circle.png'):
+        self.image_base = pygame.image.load(path_base)
+        self.image_circle = pygame.image.load(path_circle)
+        self.value = value
+        self.max_value = max_value
+        self.weight = weight
+        self.height = height
+        self.font = pygame.font.Font(FONT, SLIDER_FONT_SIZE)
+        self.text = text
+        self.text_color = color
+        self.text_box = self.font.render(text, True, color)
+        self.x, self.y = x, y
+
+    def render(self, game_manager):
+        image_base_rect = self.image_base.get_rect()
+        text_box_rect = self.text_box.get_rect()
+        self.text_box = self.font.render(
+            self.text + f': {self.value}', True, self.text_color)
+        self.image_circle_x = (self.x - 10) + self.value * \
+            self.weight // self.max_value
+        self.image_circle_y = self.y - 10
+        game_manager.display.blit(self.image_base, (self.x, self.y))
+        game_manager.display.blit(self.text_box, (self.x + (image_base_rect.width - text_box_rect.width) // 2,
+                                                  (self.y + text_box_rect.height + 10)))
+        game_manager.display.blit(
+            self.image_circle, (self.image_circle_x, self.image_circle_y))
+
+    def set_value_by_mouse(self, event):
+        if event.button == 1:
+            if self.x <= pygame.mouse.get_pos()[0] <= self.x + self.weight:
+                if self.y <= pygame.mouse.get_pos()[1] <= self.y + self.height:
+                    self.value = round(
+                        (pygame.mouse.get_pos()[0] - self.x) / self.weight * self.max_value)
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 import pygame
+from interface import *
+
 
 CONDITIONS = {'main title': 0, 'main menu': 1,
-              'settings': 2}  # condition of game
-FONT = "fonts/New Zelek.ttf"
-MAIN_FONT_SIZE = 80
-SLIDER_FONT_SIZE = 25
-RADIO_BUTTON_FONT_SIZE = 30
+              'settings': 2, 'hub': 3, 'library': 4, 'characters': 5, 'upgrades': 6, 'choose map': 7}  # condition of game
+MAPS = {'underground': 0, 'incineration plant': 1}
 CAESAR_SHIFT = 10
 
 
@@ -22,25 +21,29 @@ class GameManager:
         self.clock = pygame.time.Clock()
         self.resolution = (1920, 1080)
         self.display = pygame.display.set_mode(self.resolution)
+        self.current_condition = CONDITIONS['main title']
+        self.current_map = None
+        self.save = 'data/save.txt' if 'save.txt' in os.listdir(
+            'data') else None
+
         self.main_title = Background('images/menu/main title.png')
         self.main_menu = Background('images/menu/main menu.png')
-        self.current_condition = CONDITIONS['main title']
-        self.saves = [f'data/saves/{i}' for i in os.listdir('data/saves')]
+        self.hub_menu = Background('images/menu/hub menu.png')
         # objects
+
         self.main_buttons = {}
         self.main_buttons['continue'] = Button(
-            x=(self.resolution[0] - 800) // 2, y=100, text='Продолжить')
+            x=(self.resolution[0] - 800) // 2, y=200, text='Продолжить')
         self.main_buttons['new game'] = Button(
             x=(self.resolution[0] - 800) // 2, y=200, text='Новая игра')
-        self.main_buttons['load game'] = Button(
-            x=(self.resolution[0] - 800) // 2, y=400, text='Загрузить')
         self.main_buttons['settings'] = Button(
-            x=(self.resolution[0] - 800) // 2, y=600, text='Настройки')
+            x=(self.resolution[0] - 800) // 2, y=400, text='Настройки')
         self.main_buttons['exit'] = Button(
-            x=(self.resolution[0] - 800) // 2, y=800, text='Выход')
+            x=(self.resolution[0] - 800) // 2, y=600, text='Выход')
+
         self.settings_buttons = {}
-        self.settings_buttons['back'] = (
-            Button(x=(self.resolution[0] - 800) // 2, y=800, text='Назад'))
+        self.settings_buttons['back'] = Button(
+            x=(self.resolution[0] - 800) // 2, y=800, text='Назад')
         self.settings_buttons['volume'] = Slider(
             x=(self.resolution[0] - 800) // 2, y=200, text='Громкость', value=self.volume)
         self.settings_buttons['fps'] = Slider(
@@ -48,16 +51,103 @@ class GameManager:
         self.settings_buttons['reset'] = Button(
             x=(self.resolution[0] - 800) // 2, y=600, text='Сбросить')
 
+        self.hub_buttons = {}
+        self.hub_buttons['start'] = Button(
+            x=(self.resolution[0] - 800) // 2, y=100, text='Начать')
+        self.hub_buttons['characters'] = Button(
+            x=150, y=400, text='Персонажи')
+        self.hub_buttons['upgrades'] = Button(
+            x=970, y=400, text='Улучшения')
+        self.hub_buttons['library'] = Button(
+            x=(self.resolution[0] - 800) // 2, y=700, text='Библиотека')
+        self.hub_buttons['exit button'] = Button(
+            x=(self.resolution[0] - 150) // 2, y=900, text='', path='images/menu/exit button.png')
+
+        self.choose_map_menu_buttons = {}
+
+    def main_loop(self):
+        while True:
+            self.clock.tick(self.fps)
+            # load interface by condition
+            if self.current_condition == CONDITIONS['main title']:
+                self.main_title.render(self)
+            elif self.current_condition == CONDITIONS['main menu']:
+                self.main_menu.render(self)
+                # check up save
+                for i in self.main_buttons:
+                    if i == 'continue':
+                        if self.save != None:
+                            self.main_buttons[i].render(self)
+                    elif i == 'new game':
+                        if self.save == None:
+                            self.main_buttons[i].render(self)
+                    else:
+                        self.main_buttons[i].render(self)
+            elif self.current_condition == CONDITIONS['settings']:
+                self.main_menu.render(self)
+                for i in self.settings_buttons:
+                    self.settings_buttons[i].render(self)
+            elif self.current_condition == CONDITIONS['hub']:
+                self.hub_menu.render(self)
+                for i in self.hub_buttons:
+                    self.hub_buttons[i].render(self)
+            elif self.current_condition == CONDITIONS['choose map']:
+                self.hub_menu.render(self)
+                for i in self.choose_map_menu_buttons:
+                    self.choose_map_menu_buttons[i].render(self)
+
+            # check events
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    exit()
+                if i.type == pygame.MOUSEBUTTONDOWN:
+                    if self.current_condition == CONDITIONS['main menu']:
+                        if self.main_buttons['exit'].check_click(i):
+                            exit()
+                        if self.main_buttons['settings'].check_click(i):
+                            self.current_condition = CONDITIONS['settings']
+                        if self.main_buttons['continue'].check_click(i) or self.main_buttons['new game'].check_click(i):
+                            self.current_condition = CONDITIONS['hub']
+                            self.player_profile = PLayerProfile(
+                                'data/save.txt')
+
+                    elif self.current_condition == CONDITIONS['settings']:
+                        if self.settings_buttons['back'].check_click(i):
+                            self.fps = self.settings_buttons['fps'].get_value()
+                            self.volume = self.settings_buttons['volume'].get_value(
+                            )
+                            self.create_settings_file()
+                            self.current_condition = CONDITIONS['main menu']
+                        if self.settings_buttons['reset'].check_click(i):
+                            self.reset_settings()
+                            self.settings_buttons['fps'].set_value(self.fps)
+                            self.settings_buttons['volume'].set_value(
+                                self.volume)
+                        self.settings_buttons['volume'].set_value_by_mouse(i)
+                        self.settings_buttons['fps'].set_value_by_mouse(i)
+
+                    elif self.current_condition == CONDITIONS['hub']:
+                        if self.hub_buttons['exit button'].check_click(i):
+                            exit()
+                        if self.hub_buttons['start'].check_click(i):
+                            self.current_condition = CONDITIONS['choose map']
+
+                if i.type == pygame.KEYDOWN:
+                    if self.current_condition == CONDITIONS['main title']:
+                        self.current_condition = CONDITIONS['main menu']
+
+            pygame.display.update()
+
     def create_default_settings_file(self):
         fps = 60
         volume = 100
         data = [str(fps), str(volume)]
-        settings = open('data/default settings.txt', 'w')
-        for line in data:
-            for char in line:
-                settings.write(chr(ord(char) + CAESAR_SHIFT))
-            settings.write('\n')
-        settings.close()
+        with open('data/default settings.txt', 'w') as settings:
+            for line in data:
+                for char in line:
+                    settings.write(chr(ord(char) + CAESAR_SHIFT))
+                settings.write('\n')
+            settings.close()
 
     def create_settings_file(self):
         fps = self.fps
@@ -87,137 +177,47 @@ class GameManager:
         self.read_settings('data/default settings.txt')
         self.create_settings_file()
 
-    def main_loop(self):
-        while True:
-            self.clock.tick(self.fps)
 
-            # behaviour by condition
-            if self.current_condition == CONDITIONS['main title']:
-                self.main_title.render(self)
-            elif self.current_condition == CONDITIONS['main menu']:
-                self.main_menu.render(self)
-                # check up saves
-                if self.saves != []:
-                    self.main_buttons['continue'].render(self)
-                    self.main_buttons['new game'].y = 300
-                    self.main_buttons['load game'].y = 500
-                    self.main_buttons['settings'].y = 700
-                    self.main_buttons['exit'].y = 900
-
-                for i in self.main_buttons:
-                    if i != 'continue':
-                        self.main_buttons[i].render(self)
-            elif self.current_condition == CONDITIONS['settings']:
-                self.main_menu.render(self)
-                for i in self.settings_buttons:
-                    self.settings_buttons[i].render(self)
-
-            # check events
-            for i in pygame.event.get():
-                if i.type == pygame.QUIT:
-                    exit()
-                if i.type == pygame.MOUSEBUTTONDOWN:
-                    if self.current_condition == CONDITIONS['main menu']:
-                        if self.main_buttons['exit'].check_click(i):
-                            exit()
-                        if self.main_buttons['settings'].check_click(i):
-                            self.current_condition = CONDITIONS['settings']
-
-                    elif self.current_condition == CONDITIONS['settings']:
-                        if self.settings_buttons['back'].check_click(i):
-                            self.fps = self.settings_buttons['fps'].get_value()
-                            self.volume = self.settings_buttons['volume'].get_value()
-                            self.create_settings_file()
-                            self.current_condition = CONDITIONS['main menu']
-                        if self.settings_buttons['reset'].check_click(i):
-                            self.reset_settings()
-                            self.settings_buttons['fps'].set_value(self.fps)
-                            self.settings_buttons['volume'].set_value(
-                                self.volume)
-                        self.settings_buttons['volume'].set_value_by_mouse(i)
-                        self.settings_buttons['fps'].set_value_by_mouse(i)
-                if i.type == pygame.KEYDOWN:
-                    if self.current_condition == CONDITIONS['main title']:
-                        self.current_condition = CONDITIONS['main menu']
-
-            pygame.display.update()
-
-
-class UserInterface:
+class PLayerProfile():
     def __init__(self, path):
-        self.image = pygame.image.load(path)
+        self.path = path
+        self.create_default_save_file('data/clear save.txt')
+        if not os.path.exists(self.path):
+            self.read_save('data/clear save.txt')
+            self.create_save_file(self.path)
+        self.read_save(self.path)
 
+    def create_default_save_file(self, path):
+        self.characters = (0,)
+        self.maps = (0,)
+        self.coins = 0
+        self.create_save_file(path)
 
-class Background(UserInterface):
-    def render(self, game_manager):
-        game_manager.display.blit(self.image, self.image.get_rect())
+    def read_save(self, path):
+        save_file = open(path, 'r')
+        data = []
+        for line in save_file:
+            data.append(
+                ''.join(map(lambda char: chr(ord(char) - CAESAR_SHIFT), line)))
+        data = list(map(lambda y: y[:-1], data))
+        self.coins = int(data[0])
+        self.characters = list(map(lambda x: int(x), data[1].split(' ')))
+        self.characters = list(map(lambda x: int(x), data[2].split(' ')))
+        save_file.close()
 
-
-class Button(UserInterface):
-    def __init__(self, x=0, y=0, weight=800, height=150, text='sample', color=(0, 0, 0), path='images/menu/button.png'):
-        super().__init__(path)
-        self.weight = weight
-        self.height = height
-        self.font = pygame.font.Font(FONT, MAIN_FONT_SIZE)
-        self.text_box = self.font.render(text, True, color)
-        self.x, self.y = x, y
-
-    def render(self, game_manager):
-        image_rect = self.image.get_rect()
-        text_box_rect = self.text_box.get_rect()
-        game_manager.display.blit(self.image, (self.x, self.y))
-        game_manager.display.blit(self.text_box, (self.x + (image_rect.width - text_box_rect.width) // 2,
-                                                  (self.y + (image_rect.height - text_box_rect.height) // 2)))
-
-    def check_click(self, event):
-        if event.button == 1:
-            if self.x <= pygame.mouse.get_pos()[0] <= self.x + self.weight:
-                if self.y <= pygame.mouse.get_pos()[1] <= self.y + self.height:
-                    return True
-        return False
-
-
-class Slider(UserInterface):
-    def __init__(self, x=0, y=0, weight=800, height=20, text='sample', color=(0, 0, 0), value=100, max_value=100,
-                 path_base='images/menu/slider base.png', path_circle='images/menu/slider circle.png'):
-        self.image_base = pygame.image.load(path_base)
-        self.image_circle = pygame.image.load(path_circle)
-        self.value = value
-        self.max_value = max_value
-        self.weight = weight
-        self.height = height
-        self.font = pygame.font.Font(FONT, SLIDER_FONT_SIZE)
-        self.text = text
-        self.text_color = color
-        self.text_box = self.font.render(text, True, color)
-        self.x, self.y = x, y
-
-    def render(self, game_manager):
-        image_base_rect = self.image_base.get_rect()
-        text_box_rect = self.text_box.get_rect()
-        self.text_box = self.font.render(
-            self.text + f': {self.value}', True, self.text_color)
-        self.image_circle_x = (self.x - 10) + self.value * \
-            self.weight // self.max_value
-        self.image_circle_y = self.y - 10
-        game_manager.display.blit(self.image_base, (self.x, self.y))
-        game_manager.display.blit(self.text_box, (self.x + (image_base_rect.width - text_box_rect.width) // 2,
-                                                  (self.y + text_box_rect.height + 10)))
-        game_manager.display.blit(
-            self.image_circle, (self.image_circle_x, self.image_circle_y))
-
-    def set_value_by_mouse(self, event):
-        if event.button == 1:
-            if self.x <= pygame.mouse.get_pos()[0] <= self.x + self.weight:
-                if self.y <= pygame.mouse.get_pos()[1] <= self.y + self.height:
-                    self.value = round(
-                        (pygame.mouse.get_pos()[0] - self.x) / self.weight * self.max_value)
-
-    def get_value(self):
-        return self.value
-
-    def set_value(self, value):
-        self.value = value
+    def create_save_file(self, path):
+        characters = self.characters
+        maps = self.maps
+        coins = self.coins
+        data = [str(coins),
+                ' '.join(str(i) for i in characters),
+                ' '.join(str(i) for i in maps)]
+        settings = open(path, 'w')
+        for line in data:
+            for char in line:
+                settings.write(chr(ord(char) + CAESAR_SHIFT))
+            settings.write('\n')
+        settings.close()
 
 
 def main():
